f2e7170d828618939ce461d37f45b447e18d354c
Arthur Kenichi Condino Odaguiri <46831317+Arthur-Kenichi-Condino@users.noreply.github.com>
2022-02-01 13:39:04 -0300

    ...

--- Assets/AKCondinoO/Voxels/VoxelSystem.cs
+++ Assets/AKCondinoO/Voxels/VoxelSystem.cs
@@ -298,7 +298,7 @@ namespace AKCondinoO.Voxels{
          }
          if(DEBUG_EDIT){
             DEBUG_EDIT=false;
-          Logger.Debug("DEBUG_EDIT_AT:"+DEBUG_EDIT_AT);
+          //Logger.Debug("DEBUG_EDIT_AT:"+DEBUG_EDIT_AT);
           EditTerrain(
            DEBUG_EDIT_AT,
            DEBUG_EDIT_MODE,
@@ -507,20 +507,95 @@ namespace AKCondinoO.Voxels{
           internal readonly StreamWriter editsFileStreamWriter;
           internal readonly StreamReader editsFileStreamReader;
            internal readonly StringBuilder editsStringBuilder=new StringBuilder();
+         static readonly ConcurrentQueue<Dictionary<Vector3Int,(double,MaterialId)>>chunkDataPool=new ConcurrentQueue<Dictionary<Vector3Int,(double,MaterialId)>>();
+         readonly Dictionary<int,Dictionary<Vector3Int,(double density,MaterialId materialId)>>readData=new Dictionary<int,Dictionary<Vector3Int,(double,MaterialId)>>();
+         readonly Dictionary<int,Dictionary<Vector3Int,(double density,MaterialId materialId)>>saveData=new Dictionary<int,Dictionary<Vector3Int,(double,MaterialId)>>();
          internal TerrainEditingMultithreaded(){
           editsFileStream=new FileStream(VoxelSystem.editsFile,FileMode.OpenOrCreate,FileAccess.ReadWrite,FileShare.ReadWrite);
            editsFileStreamWriter=new StreamWriter(editsFileStream);
            editsFileStreamReader=new StreamReader(editsFileStream);
          }
-         static readonly ConcurrentQueue<Dictionary<Vector3Int,(double,MaterialId)>>chunkDataPool=new ConcurrentQueue<Dictionary<Vector3Int,(double,MaterialId)>>();
-         readonly Dictionary<int,Dictionary<Vector3Int,(double density,MaterialId materialId)>>readData=new Dictionary<int,Dictionary<Vector3Int,(double,MaterialId)>>();
-         readonly Dictionary<int,Dictionary<Vector3Int,(double density,MaterialId materialId)>>saveData=new Dictionary<int,Dictionary<Vector3Int,(double,MaterialId)>>();
+         protected override void Cleanup(){
+          foreach(var chunkData in readData){chunkData.Value.Clear();chunkDataPool.Enqueue(chunkData.Value);}
+          readData.Clear();
+          foreach(var chunkData in saveData){chunkData.Value.Clear();chunkDataPool.Enqueue(chunkData.Value);}
+          saveData.Clear();
+         }
          protected override void Execute(){
-          Logger.Debug("TerrainEditingMultithreaded Execute");
+          //Logger.Debug("TerrainEditingMultithreaded Execute");
           lock(VoxelSystem.terrainSynchronization){
-           Logger.Debug("container.requests.Count:"+container.requests.Count);
+           //Logger.Debug("container.requests.Count:"+container.requests.Count);
            container.dirty.Clear();
            while(container.requests.Count>0){var editRequest=container.requests.Dequeue();
+            Vector3    center    =editRequest.center;
+            Vector3Int size      =editRequest.size;
+            double     density   =editRequest.density;
+            MaterialId material  =editRequest.material;
+            int        smoothness=editRequest.smoothness;
+            switch(editRequest.mode){
+             default:{
+              float sqrt_yx_1=Mathf.Sqrt(Mathf.Pow(size.y,2)+Mathf.Pow(size.x,2));
+              float sqrt_xz_1=Mathf.Sqrt(Mathf.Pow(size.x,2)+Mathf.Pow(size.z,2));
+              float sqrt_zy_1=Mathf.Sqrt(Mathf.Pow(size.z,2)+Mathf.Pow(size.y,2));
+               float sqrt_yx_xz_1=Mathf.Sqrt(Mathf.Pow(sqrt_yx_1,2)+Mathf.Pow(sqrt_xz_1,2));
+                float sqrt_yx_xz_zy_1=Mathf.Sqrt(Mathf.Pow(sqrt_yx_xz_1,2)+Mathf.Pow(sqrt_zy_1,2));
+              float sqrt_yx_2;
+              float sqrt_xz_2;
+              float sqrt_zy_2;
+              Vector2Int cCoord1=vecPosTocCoord(center ),        cCoord3;
+              Vector2Int cnkRgn1=cCoordTocnkRgn(cCoord1),        cnkRgn3;
+              Vector3Int vCoord1=vecPosTovCoord(center ),vCoord2,vCoord3;
+              for(int y=0;y<size.y+smoothness;++y){for(vCoord2=new Vector3Int(vCoord1.x,vCoord1.y-y,vCoord1.z);vCoord2.y<=vCoord1.y+y;vCoord2.y+=y*2){
+               if(vCoord2.y>=0&&vCoord2.y<Height){
+              for(int x=0;x<size.x+smoothness;++x){for(vCoord2.x=vCoord1.x-x                                  ;vCoord2.x<=vCoord1.x+x;vCoord2.x+=x*2){
+                sqrt_yx_2=Mathf.Sqrt(Mathf.Pow(y,2)+Mathf.Pow(x,2));
+              for(int z=0;z<size.z+smoothness;++z){for(vCoord2.z=vCoord1.z-z                                  ;vCoord2.z<=vCoord1.z+z;vCoord2.z+=z*2){
+                cCoord3=cCoord1;
+                cnkRgn3=cnkRgn1;
+                vCoord3=vCoord2;
+                if(vCoord2.x<0||vCoord2.x>=Width||
+                   vCoord2.z<0||vCoord2.z>=Depth
+                ){
+                 ValidateCoord(ref cnkRgn3,ref vCoord3);
+                 cCoord3=cnkRgnTocCoord(cnkRgn3);
+                }
+                int cnkIdx3=GetcnkIdx(cCoord3.x,cCoord3.y);
+                sqrt_xz_2=Mathf.Sqrt(Mathf.Pow(x,2)+Mathf.Pow(z,2));
+                sqrt_zy_2=Mathf.Sqrt(Mathf.Pow(z,2)+Mathf.Pow(y,2));
+                double resultDensity;
+                if(y>=size.y||x>=size.x||z>=size.z){
+                 if(y>=size.y&&x>=size.x&&z>=size.z){
+                  float sqrt_yx_xz_2=Mathf.Sqrt(Mathf.Pow(sqrt_yx_2,2)+Mathf.Pow(sqrt_xz_2,2));
+                   float sqrt_yx_xz_zy_2=Mathf.Sqrt(Mathf.Pow(sqrt_yx_xz_2,2)+Mathf.Pow(sqrt_zy_2,2));
+                  resultDensity=density*(1f-(sqrt_yx_xz_zy_2-sqrt_yx_xz_1)/(sqrt_yx_xz_zy_2));
+                 }else if(y>=size.y&&x>=size.x){resultDensity=density*(1f-(sqrt_yx_2-sqrt_yx_1)/(sqrt_yx_2));
+                 }else if(x>=size.x&&z>=size.z){resultDensity=density*(1f-(sqrt_xz_2-sqrt_xz_1)/(sqrt_xz_2));
+                 }else if(z>=size.z&&y>=size.y){resultDensity=density*(1f-(sqrt_zy_2-sqrt_zy_1)/(sqrt_zy_2));
+                 }else if(y>=size.y){resultDensity=density*(1f-(y-size.y)/(float)y)*1.414f;
+                 }else if(x>=size.x){resultDensity=density*(1f-(x-size.x)/(float)x)*1.414f;
+                 }else if(z>=size.z){resultDensity=density*(1f-(z-size.z)/(float)z)*1.414f;
+                 }else{
+                  resultDensity=0d;
+                 }
+                }else{
+                 resultDensity=density;
+                }
+                //  TO DO: get current file data to merge
+                Voxel currentVoxel;
+                if(readData.ContainsKey(cnkIdx3)&&readData[cnkIdx3].ContainsKey(vCoord3)){
+                 (double density,MaterialId materialId)voxelData=readData[cnkIdx3][vCoord3];
+                }else{
+                }
+               if(z==0){break;}
+              }}
+               if(x==0){break;}
+              }}
+               }
+               if(y==0){break;}
+              }}
+              break;
+             }
+            }
            }           
            foreach(var syn in VoxelSystem.terrainSynchronization)Monitor.Enter(syn.Value);
            try{
